8efa0983c7c008a20755e6aec49cdabab820338c
Chris Butler <chris.butler@redhat.com>
2024-08-11 13:38:03 +1000

    fix: Add debugging
    Signed-off-by: Chris Butler <chris.butler@redhat.com>

--- micro/minisleep/buildscript.sh
+++ micro/minisleep/buildscript.sh
@@ -6,20 +6,14 @@ mountpoint=$(buildah mount ${container})
 
 
 # Install dependencies into the runtime container
-buildah run ${builder} --volume $mountpoint:/hello:z -- bash -C <<EOF
-    yum install -y \
-        --installroot /hello --releasever 9 \
-        --setopt install_weak_deps=false \
-        --nodocs \
-        core-utils
+buildah -vvv  run ${builder} --volume $mountpoint:/hello:z -- bash -C <<EOF
     yum remove -y --installroot /hello --releasever 9 \
         --setopt install_weak_deps=false \
         --nodocs \
         bash
-
     yum clean all --installroot /hello
 EOF
 
-buildah commit --format docker ${container} minisleep
+buildah commit --format docker ${container} microsleep
 buildah unmount ${container}
 
