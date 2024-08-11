9613fef1f8f98ea4f68aaa140dd3201a70ed9c0b
Chris Butler <chris.butler@redhat.com>
2024-08-11 13:50:18 +1000

    fix: Add shell to script
    Signed-off-by: Chris Butler <chris.butler@redhat.com>

--- micro/minisleep/buildscript.sh
+++ micro/minisleep/buildscript.sh
@@ -5,10 +5,10 @@ container=$(buildah from registry.redhat.io/ubi9-micro)
 mountpoint=$(buildah mount ${container})
 
 
-
+buildah run ${builder} -- touch /hello
 
 # Install dependencies into the runtime container
-buildah --log-level debug  run ${builder} --volume $mountpoint:/hello:z -- bash -C <<EOF
+buildah run ${builder} --volume $mountpoint:/hello:z -- bash -C <<EOF
     yum remove -y --installroot /hello --releasever 9 \
         --setopt install_weak_deps=false \
         --nodocs \
