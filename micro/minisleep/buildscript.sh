e5ba9f748330f06f24a6610f656616b83c592e44
Chris Butler <chris.butler@redhat.com>
2024-08-11 13:40:06 +1000

    fix: correct debugging
    Signed-off-by: Chris Butler <chris.butler@redhat.com>

--- micro/minisleep/buildscript.sh
+++ micro/minisleep/buildscript.sh
@@ -1,3 +1,4 @@
+set -e
 builder=$(buildah from registry.redhat.io/ubi9/ubi)
 container=$(buildah from registry.redhat.io/ubi9-micro)
 mountpoint=$(buildah mount ${container})
@@ -6,7 +7,7 @@ mountpoint=$(buildah mount ${container})
 
 
 # Install dependencies into the runtime container
-buildah -vvv  run ${builder} --volume $mountpoint:/hello:z -- bash -C <<EOF
+buildah --log-level debug  run ${builder} --volume $mountpoint:/hello:z -- bash -C <<EOF
     yum remove -y --installroot /hello --releasever 9 \
         --setopt install_weak_deps=false \
         --nodocs \
